6c331c78babe8540e03e4eacfed073cee7dc29cf
mcp656 <mcp656@alumni.ku.dk>
2023-04-13 22:51:50 +0200

    Big changes!

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,3 +1,24 @@
-def pr(x):
-    return 2*x
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+def norm_col(col):
+    col_sum = col.sum()
+    norm_col = col / col_sum
+    return norm_col
+
+
+def cum_ret_plot(data, stock, ax_data=None):
+    if ax_data is None:
+        ax_data = data
+
+    # first plot of y using ax_data
+    ax = ax_data.plot(y=stock) 
+    # second plot of OMXC25 which contains the positional arg: ax = ax
+    data.plot(y='^OMXC25',
+              ax=ax,
+              title = f'Figure 1: Cumulative Return of {stock} compared to OMXC25',
+              ylabel = 'Cumulative Return')
+    # display plot
+    plt.show()
