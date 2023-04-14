f373f3a57148a53572523a1cbb714ee9ae2c7724
mcp656 <mcp656@alumni.ku.dk>
2023-04-14 19:36:00 +0200

    Changes

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -4,28 +4,29 @@ import matplotlib.pyplot as plt
 
 
 def calculate_returns(data):
-    # Calculate monthly returns
+    # calculate monthly returns
     data_r = data.pct_change()
 
-    # Calculate cumulative returns
+    # calculate cumulative returns
     data_cr = (1 + data_r).cumprod()
 
     return data_r, data_cr
 
 
-def cum_ret_plot(data, stock, fig = 1, ax_data=None):
+def cum_ret_plot(data, stock, ref, fig = 1, ax_data=None):
     if ax_data is None:
         ax_data = data
 
     # first plot of y using ax_data
     ax = ax_data.plot(y=stock) 
     # second plot of OMXC25 which contains the positional arg: ax = ax
-    data.plot(y='^OMXC25',
+    data.plot(y=ref,
               ax=ax,
-              title = f'Figure {fig}: Cumulative Return of {stock} compared to OMXC25',
+              title = f'Figure {fig}: Cumulative Return of {stock} compared to {ref}',
               ylabel = 'Cumulative Return')
     # display plot
     plt.show()
+
 
 def norm_col(col):
     col_sum = col.sum()
