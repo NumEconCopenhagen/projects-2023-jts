8ea6fb430b6e3740e043388759c79962be4f0ef5
mcp656 <mcp656@alumni.ku.dk>
2023-05-03 16:36:16 +0200

    New files, modifications etc.

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,73 @@
+import numpy as np
 from scipy import optimize
+import random
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
-
+def n_ss(variables, s_K, s_H, n, g, delta, alpha, varphi):
+    """
     Args:
-        c (float): costs
-        alpha (float): parameter
-
+    variables (list or tuple): Contains two variables to be solved for, capital and human capital
+    s_K      (float): Savings rate in capital
+    s_H      (float): Savings rate in human capital
+    n        (float): Population growth rate
+    g        (float): TFP growth rate
+    delta    (float): Depreciation rate
+    alpha    (float): Capital share in the production function
+    varphi   (float): Human capital share in the production function
+    
     Returns:
-        result (RootResults): the solution represented as a RootResults object.
-
-    """ 
+    Capital and human capital in steady state
+    """
+    #Variables to be solved for, capital and human capital
+    k, h = variables
     
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+    # Check for edge cases
+    if k <= 0 or h <= 0:
+        return [1e10, 1e10]  # Return a large residual to indicate a poor solution
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
+    # Sets the Solow equations in steady state
+    n_ss_solow_k = (1 / ((1 + n) * (1 + g))) * (s_K * k**alpha * h**varphi - (n + g + delta + n * g) * k)
+    n_ss_solow_h = (1 / ((1 + n) * (1 + g))) * (s_H * k**alpha * h**varphi - (n + g + delta + n * g) * h)
+
+    return [n_ss_solow_k, n_ss_solow_h]
+
+
+def multi_start(num_guesses=100, bounds=[0.1, 10], fun=n_ss, args= None, method='hybr'):
+    """
+    Performs multi-start optimization to find the steady state solutions for k and h.
     
-    return result
+    Args:
+    num_guesses     (int): The number of random initial guesses, default=100
+    bounds        (tuple): The bounds for the random initial guesses, default=[0.1, 10]
+    fun        (function): The function to be optimized, default=n_ss
+    args          (tuple): The tuple of arguments for the function, default= None
+    method       (method): The optimization method to use, default='hybr'
+    
+    Returns:
+    Prints the steady state values for k and h, and the residual of the function
+    """
+    # Initialize the smallest residual as infinity
+    smallest_residual = np.inf
+
+    # Generate a list of random numbers within the specified bounds
+    random_samples = list(np.random.uniform(low=bounds[0], high=bounds[1], size=num_guesses))
+
+    # Loop through each random initial guess
+    for i in range(num_guesses):
+        # Select a random pair of numbers from the list of random samples
+        initial_guess = random.sample(random_samples, 2)
+
+        # Solve the optimization problem with the current initial guess
+        sol = optimize.root(fun=fun, x0=initial_guess, args=args, method=method)
+        
+        # Calculate the residual norm (Euclidean norm) of the current solution
+        residual_norm = np.linalg.norm(sol.fun)
+
+        # If the residual norm is smaller than the current smallest residual, update the steady state of k and h and the smallest residual
+        if residual_norm < smallest_residual:
+            smallest_residual = residual_norm
+            ms_ss_k, ms_ss_h = sol.x
+
+    # Print the steady-state values for capital and human capital
+    print(f'Muti-Start Solution:\n\
+There are {ms_ss_k:.3f} units of capital, and {ms_ss_h:.3f} units of human capital in steady state\n\
+The functions residual is {smallest_residual}')
