8be65802d8ed5b5c01ea05d08aed72db4277eeab
mcp656 <mcp656@alumni.ku.dk>
2023-05-23 22:44:38 +0200

    Final dataproject

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -4,70 +4,129 @@ import matplotlib.pyplot as plt
 from adjustText import adjust_text
 import seaborn as sns
 
-def calculate_returns(data):
-    # calculates monthly returns using the pct_change() function
+def calculate_returns(data: pd.DataFrame):
+    """
+    Calculates the monthly and cumulative returns for a DataFrame of stock prices
+
+    Parameters:
+    data       (pd.DataFrame): DataFrame of stock prices
+
+    Returns:
+    data_r     (pd.DataFrame): DataFrame of stock returns
+    data_cr    (pd.DataFrame): DataFrame of cumulative stock returns
+    """
+    # Calculate monthly returns using the pct_change() function
     data_r = data.pct_change()
 
-    # calculates cumulative returns using the cumprod() function
+    # Calculate cumulative returns using the cumprod() function
     data_cr = (1 + data_r).cumprod()
-    
-    # returns monthly and cumulative returns
+
+    # Returns monthly and cumulative stock returns
     return data_r, data_cr
 
 
-def calculate_portfolio_returns(data, weights):
-    # calculate weighted returns
+def calculate_portfolio_returns(data: pd.DataFrame, weights: pd.DataFrame):
+    """
+    Calculates portfolio and cumulative returns based on returns and weights for each stock
+
+    Parameters:
+    data        (pd.DataFrame): DataFrame of stock returns
+    weights     (pd.DataFrame): DataFrame of weights for each stock
+
+    Returns:
+    port_r         (pd.Series): Series of portfolio returns
+    cum_port_r     (pd.Series): Series of cumulative portfolio returns
+    """
+    # Calculate weighted returns
     weighted_r = data * weights
 
-    # calculate portfolio returns
-    port_r = weighted_r.sum(axis=1)
+    # Calculate portfolio return by summing up the weighted returns across all stocks
+    port_r = weighted_r.sum(axis = 1)
 
-    # calculate cumulative return
+    # Calculate cumulative return of the portfolio
     cum_port_r = (1 + port_r).cumprod()
 
-    # return portfolio returns and cumulative returns
+    # Return portfolio and cumulative portfolio returns
     return port_r, cum_port_r
 
 
-def cum_ret_plot(data, stock, ref, fig = 1, ax_data=None):
-    # set ax_data to pd.dataframe for a noninteractive comparison of two stocks
+def cum_ret_plot(data: pd.DataFrame, stock: str, ref: str, fig: int = 1, ax_data: pd.DataFrame = None):
+    """
+    Plots the cumulative return of a stock against a reference 'index'
+
+    Parameters:
+    data        (pd.DataFrame): DataFrame of cumulative stock returns
+    stock                (str): Which stock to plot
+    ref                  (str): Reference/Index
+    fig                  (int): Figure no. in title, (defualt = 1)
+    ax_data     (pd.DataFrame): Only used for non-interactive plots, (default = None).
+
+    Returns:
+    A plot of a stocks cumulative return compared to a reference/index
+    """
+    # If ax_data is None, set it to the input data for an interactive plot
     if ax_data is None:
         ax_data = data
 
-    # first plot of y using ax_data = data for interactive
-    ax = ax_data.plot(y=stock) 
-    # second plot of the reference 'index', contains the positional arg: ax = ax
-    data.plot(y=ref,
-              # only for noninteractive comparison otherwise ax = data
-              ax=ax,
-              # set title and ylabel
+    # Plot the specified stock using ax_data
+    ax = ax_data.plot(y = stock)
+
+    # Plot the reference 'index' on the same axes (by specifying ax=ax)
+    data.plot(y = ref, ax = ax, 
               title = f'Figure {fig}: Cumulative Return of {stock} compared to {ref}',
               ylabel = 'Cumulative Return')
-    # display plot
-    plt.show()
+
+    # Display plot
+    plt.show();
 
 
-def plot_scatter_with_labels(ax, x, y, labels, title, xlabel, ylabel):
-    # create the scatter plot with a regression line
-    sns.regplot(x=x, y=y, scatter=False, ax=ax)
+def plot_scatter_with_labels(ax: plt.Axes, x: list, y: list, labels: list, title: str, xlabel: str, ylabel: str):
+    """
+    Plots a scatter-plot with automatically adjusted labels for each point, a trend line and labels for the title and axes
+
+    Parameters:
+    ax          (plt.Axes): The Axes object to draw the plot onto
+    x               (list): List of x-axis values
+    y               (list): List of y-axis values
+    labels          (list): List of labels for the data points
+    title            (str): The title for the graph
+    xlabel           (str): The label for the x-axis
+    ylabel           (str): The label for the y-axis
+
+    Returns:
+    A scatter-plot with automatically adjusted labels for each point, a trend line and labels for the title and axes
+    """
+    # Create the scatter plot with a trend line
     ax.scatter(x, y)
+    sns.regplot(x = x, y = y, scatter = False, ax = ax)
 
-    # add a list of text labels for each data point using ax.text and a for loop
-    texts = [ax.text(x_pos, y_pos, f'{lab}', fontsize=8, ha='center') for (x_pos, y_pos, lab) in zip(x, y, labels)]
+    # Add labels to each data point using ax.text in a for loop
+    texts = [ax.text(x_pos, y_pos, lab, fontsize = 8, ha = 'center') for x_pos, y_pos, lab in zip(x, y, labels)]
 
-    # adjust the text labels for each data point automatically
-    adjust_text(texts, arrowprops=dict(arrowstyle='-', color='k', lw=0.5), ax=ax)
+    # Automatically adjust the labels to avoid overlapping using adjust_text
+    adjust_text(texts, arrowprops=dict(arrowstyle = '-', color = 'k', lw = 0.5), ax = ax)
 
-    # set title and labels
-    ax.set_title(title, fontsize=12)
-    ax.set_xlabel(xlabel, fontsize=12)
-    ax.set_ylabel(ylabel, fontsize=12)
+    # Set the title and labels for the axes
+    ax.set_title(title, fontsize = 12)
+    ax.set_xlabel(xlabel, fontsize = 12)
+    ax.set_ylabel(ylabel, fontsize = 12)
 
 
-def normalize_column(col):
-    # set col_sum to the sum of the column
+def normalize_column(col: pd.Series):
+    """
+    Normalizes a column to one
+
+    Parameters:
+    col        (pd.Series): The column to be normalized
+
+    Returns:
+    norm_col   (pd.Series): A normalized column
+    """
+    # Calculate the sum of the column
     col_sum = col.sum()
-    # for each value in column divide by col_sum
+
+    # Normalize each value in the column by dividing it by the sum
     norm_col = col / col_sum
-    # returns normalized column
+
+    # Return the normalized column
     return norm_col
